aed5d18cd279481265a47f2f9483090030fb66b5
Sovannrathh <sovannrath092@gmail.com>
2025-12-02 00:31:30 +0700

    add additional routes

--- web_frontend/app/routes.ts
+++ web_frontend/app/routes.ts
@@ -3,4 +3,10 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("trip_plan", "routes/trip_plan.tsx"),
+  route("/place/:id", "routes/place.tsx"),
+  route("/hotels", "routes/hotels.tsx"),
+  route("/hotel/:id", "routes/hotel.tsx"),
+  route("/hotel/:id/rooms", "routes/rooms.tsx"),
+  route("/hotel/:id/room/:roomId", "routes/room.tsx"),
+  route("/restaurant/:id", "routes/restaurant.tsx"),
 ] satisfies RouteConfig;
